fe4d8ca9fc76f92ea9d16f441b31c93138753738
saikishore222 <saikishore.chsk@gmail.com>
2020-05-26 11:26:35 +0530

    django image upload

---
+++ imageupload/mysite/imageupload/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse 
+from django.shortcuts import render, redirect 
+from .forms import *
+
+# Create your views here. 
+def hotel_image_view(request): 
+
+	if request.method == 'POST': 
+		form = HotelForm(request.POST, request.FILES) 
+
+		if form.is_valid(): 
+			form.save() 
+			return redirect('success') 
+	else: 
+		form = HotelForm() 
+	return render(request, 'image.html', {'form' : form}) 
+
+
+def success(request): 
+	return HttpResponse('successfully uploaded') 
+from django.shortcuts import render
+
+# Create your views here.
